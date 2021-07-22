b96b3b75224e95962474f0d6da5899a9c613bab1
Metages Gashe <metagesgashe@gmail.com>
2021-07-21 22:56:27 -0400

    add media query and event listener

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-const downld = document.getElementById("downld");
+const downld = document.getElementsByClassName("downld")[0];
 downld.addEventListener('click', function(e) {
     window.print(e);
+})
+
+const terms = document.getElementById("terms")
+terms.addEventListener('click', function(e) {
+    const termsConditions = document.getElementById("terms-conditions");
+    if (termsConditions.style.display === "block"){
+        termsConditions.style.display = "none";
+    } else {
+        termsConditions.style.display = "block";
+    }
 })
